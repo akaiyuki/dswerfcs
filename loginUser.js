c8e3755c2ca0f4775a454c47c5cae517310f4ec1
AV Dev <smartwavedev@Smartwaves-MacBook-Pro.local>
2016-08-25 15:00:23 +0800

    todoav backend

--- loginUser.js
+++ loginUser.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
+var mime = require("mime");
+
 var bodyParser = require('body-parser');
 
 
@@ -234,3 +239,36 @@ var server = app.listen(8081, function () {
   console.log("Example app listening at http://%s:%s", host, port)
 
 })
+
+
+// function send404(response) {
+//   response.writeHead(404, {"Content-type" : "text/plain"});
+//   response.write("Error 404: resource not found");
+//   response.end();
+// }
+
+// function sendPage(response, filePath, fileContents) {
+//   response.writeHead(200, {"Content-type" : mime.lookup(path.basename(filePath))});
+//   response.end(fileContents);
+// }
+
+
+// function serverWorking(response, absPath) {
+//   fs.exists(absPath, function(exists) {
+//     if (exists) {
+//       fs.readFile(absPath, function(err, data) {
+//         if (err) {
+//           send404(response)
+//         } else {
+//           sendPage(response, absPath, data);
+//         }
+//       });
+//     } else {
+//       send404(response);
+//     }
+//   });
+// }
+
+
+
+
