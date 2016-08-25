0c03dd67ef379e6acb2b171da3544b7ddb0cc4d0
AV Dev <smartwavedev@Smartwaves-MacBook-Pro.local>
2016-08-25 18:33:00 +0800

    edited

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-var express = require('express')
-var app = express()
+// var express = require('express')
+// var app = express()
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+// app.set('port', (process.env.PORT || 5000))
+// app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+// app.get('/', function(request, response) {
+//   response.send('Hello World!')
+// })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+// app.listen(app.get('port'), function() {
+//   console.log("Node app is running at localhost:" + app.get('port'))
+// })
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
